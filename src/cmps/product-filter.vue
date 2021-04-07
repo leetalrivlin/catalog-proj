daafe1cf5b170d1827ad423244c4a2e0f5b86f1f
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-04-07 17:43:14 +0300

    select css

--- src/cmps/product-filter.vue
+++ src/cmps/product-filter.vue
@@ -1,30 +1,28 @@
 <template>
-  <form action="">
-      <select name="Gender" v-model="filterBy.gender" @change="setFilter">
-                    <option value="him">Him</option>
-                    <option value="her">Her</option>
-                    <option value="both">Both</option>
-                </select>
+  <form @submit.prevent class="flex justify-center product-filter-container">
+    <select name="Gender" v-model="filterBy.gender" @change="setFilter" class="gender-select">
+      <option value="him">Him</option>
+      <option value="her">Her</option>
+      <option value="both">Both</option>
+    </select>
   </form>
 </template>
 
 <script>
 export default {
-     data() {
-        return {
-            filterBy: {
-                gender: 'both',
-                
-            }
-        }
+  data() {
+    return {
+      filterBy: {
+        gender: 'both',
+      },
+    };
+  },
+
+  methods: {
+    setFilter() {
+      const filterBy = JSON.parse(JSON.stringify(this.filterBy.gender));
+      this.$emit('productsToShow', filterBy);
     },
-
-     methods: {
-        setFilter() {
-            const filterBy = JSON.parse(JSON.stringify(this.filterBy.gender))
-            this.$emit( 'productsToShow', filterBy)
-
-    }
-     }
+  },
 };
 </script>
