5655b9bd5b51c910bfb17bf6874f0c3517457f90
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-04-07 14:47:04 +0300

    changes

--- src/cmps/product-list.vue
+++ src/cmps/product-list.vue
@@ -1,11 +1,19 @@
 <template>
   <section>
-      <h1>Product list</h1>
+    <h1>Product list</h1>
+    <ul>
+      <li>
+        <product-preview />
+      </li>
+    </ul>
   </section>
 </template>
 
 <script>
+import productPreview from './product-preview.vue';
 export default {
-
-}
+  components: {
+    productPreview,
+  },
+};
 </script>
