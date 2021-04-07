c64b06b3d0f66281062f908328e3cc0de27aeea6
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-04-07 13:58:22 +0300

    adding products-app page

--- src/cmps/main-header.vue
+++ src/cmps/main-header.vue
@@ -1,10 +1,11 @@
 <template>
   <section class="main-header-container main-layout flex align-center space-between">
     <h2>Logo</h2>
-    <nav class="flex">
-      <a href="">Features</a>
-      <a href="">Team</a>
-      <a href="">Signin</a>
+    <nav class="flex align-center">
+      <router-link to="/">Home</router-link> 
+      <span>|</span>
+      <router-link to="/product">Products</router-link>
+
     </nav>
   </section>
 </template>
