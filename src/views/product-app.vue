8409aac2d618c53572852841257e7f2b1bcb5306
lizamir <lizamir2@gmail.com>
2021-04-07 14:41:17 +0300

    service

--- src/views/product-app.vue
+++ src/views/product-app.vue
@@ -1,15 +1,33 @@
 <template>
   <section>
-      <h1>Products</h1>
-      <product-list />
+    <h1>Products</h1>
+    <product-list />
   </section>
 </template>
 
 <script>
-import productList from '../cmps/product-list.vue';
+import productList from "../cmps/product-list.vue";
+import { productService } from '../services/productService.js';
 export default {
-components: {
+  data() {
+    return {
+      products: null,
+    };
+  },
+  components: {
     productList,
-}
-}
+  },
+
+  methods: {
+    async loadProducts() {
+      const products = await productService.query();
+      this.products = products;
+      console.log(this.products);
+    },
+  },
+
+  created() {
+    this.loadProducts();
+  },
+};
 </script>
