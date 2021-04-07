3fd5392da41f17bfebe4e2854540859b11368dd0
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-04-07 14:06:40 +0300

    product list

--- src/views/product-app.vue
+++ src/views/product-app.vue
@@ -1,11 +1,15 @@
 <template>
   <section>
-      <h1>Product App</h1>
+      <h1>Products</h1>
+      <product-list />
   </section>
 </template>
 
 <script>
+import productList from '../cmps/product-list.vue';
 export default {
-
+components: {
+    productList,
+}
 }
 </script>
