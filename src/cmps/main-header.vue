e03f99f9ff02473c7eb5ad624588bfb199f1a2ee
leetalrivlin <41389353+leetalrivlin@users.noreply.github.com>
2021-04-07 13:36:26 +0300

    first push

---
+++ src/cmps/main-header.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="main-header-container main-layout flex align-center space-between">
+    <h2>Logo</h2>
+    <nav class="flex">
+      <a href="">Features</a>
+      <a href="">Team</a>
+      <a href="">Signin</a>
+    </nav>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
